257e0dc8c3231daacd355014c683343768f384d9
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2014-06-05 16:54:24 +0200

    Added script for moving the pikeman

---
+++ Assets/MovePikeman.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePikeman : MonoBehaviour {
+
+	public float MovementSpeed = 1f;
+	public float RotationSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = Vector3.zero;
+		if (Input.GetKey(KeyCode.W)) {
+			direction = this.transform.forward;
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			direction += -this.transform.forward;
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			this.transform.Rotate(this.transform.up, RotationSpeed);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			this.transform.Rotate(this.transform.up, -RotationSpeed);
+		}
+
+		if (direction.sqrMagnitude > 0f) {
+			direction = direction.normalized;
+			Vector3 speed = direction * Time.deltaTime * MovementSpeed;
+
+			this.transform.position = speed + this.transform.position;
+
+			this.GetComponent<Animator>().SetFloat("runSpeed", speed.magnitude);
+		}
+	}
+}
